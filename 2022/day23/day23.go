dbbdf7cdf9bc392e66eed2dd5a969b297e2fa32f
Blarc <jakob.malezic@gmail.com>
2022-12-24 09:50:57 +0100

    [feat] Add solution for 2022/day23.

--- 2022/day23/day23.go
+++ 2022/day23/day23.go
@@ -60,7 +60,7 @@ func findAdjacent(elf Elf, elfs map[string]Elf) ([][]int, bool) {
 	// north, south, west, east
 
 	// 0, 1, 2
-	// 3, 4, 5
+	// 3, 4, 5omm
 	// 6, 7, 8
 
 	r = [][]int{r[0], r[1], r[2], r[6], r[7], r[8], r[0], r[3], r[6], r[2], r[5], r[8]}
@@ -98,6 +98,7 @@ func part1(input string) int {
 	}
 
 	// show(elfs, maxY, maxX, minY, minX)
+	fmt.Println(len(elfs))
 
 	globalDir := 1
 	for i := 0; i < 10; i++ {
@@ -117,7 +118,14 @@ func part1(input string) int {
 					elf.proposedY = proposal[0]
 					elf.proposedX = proposal[1]
 					elf.hasProposal = true
-					proposals[createKey(proposal[0], proposal[1])] += 1
+
+					proposalKey := createKey(proposal[0], proposal[1])
+					_, pe := proposals[proposalKey]
+					if !pe {
+						proposals[proposalKey] = 1
+					} else {
+						proposals[proposalKey] += 1
+					}
 				} else {
 					elf.hasProposal = false
 				}
@@ -126,6 +134,8 @@ func part1(input string) int {
 			elfs[k] = elf
 		}
 
+		fmt.Println(1, len(elfs))
+
 		newElfs := make(map[string]Elf)
 		for _, elf := range elfs {
 			if elf.hasProposal {
@@ -133,8 +143,9 @@ func part1(input string) int {
 				if pe && p == 1 {
 					elf.y = elf.proposedY
 					elf.x = elf.proposedX
-					elf.hasProposal = false
 				}
+				elf.hasProposal = false
+
 			}
 
 			if elf.y > maxY {
@@ -152,15 +163,21 @@ func part1(input string) int {
 				minX = elf.x
 			}
 
+			_, elfExists := newElfs[createKey(elf.y, elf.x)]
+			if elfExists {
+				panic("WTF")
+			}
+
 			newElfs[createKey(elf.y, elf.x)] = elf
 
 		}
 
 		elfs = newElfs
+		fmt.Println(2, len(elfs))
 
 		globalDir += 3
 		fmt.Printf("Round %d\n", i+1)
-		show(elfs, maxY, maxX, minY, minX)
+		// show(elfs, maxY, maxX, minY, minX)
 	}
 
 	// show(elfs, maxY, maxX, minY, minX)
@@ -168,11 +185,114 @@ func part1(input string) int {
 	// 3882
 	// 1116
 
+	fmt.Println(len(elfs))
 	return (maxX-minX+1)*(maxY-minY+1) - len(elfs)
 }
 
 func part2(input string) int {
-	return 2
+	lines := strings.Split(input, "\n")
+
+	elfs := make(map[string]Elf)
+
+	minY, minX := 0, 0
+	maxY, maxX := len(lines), len(lines[0])
+
+	for y := 0; y < len(lines); y++ {
+		line := lines[y]
+		for x := 0; x < len(line); x++ {
+			if line[x] == '#' {
+				elfs[createKey(y, x)] = Elf{y, x, y, x, false}
+			}
+		}
+	}
+
+	globalDir := 1
+	index := 1
+	for {
+
+		minY, minX = 1000, 1000
+		maxY, maxX = -1000, -1000
+
+		proposals := make(map[string]int)
+
+		for k, elf := range elfs {
+			adjacent, exists := findAdjacent(elf, elfs)
+			if exists {
+				// fmt.Println("elf", elf.y, elf.x)
+				proposal := findProposal(globalDir, adjacent)
+				if len(proposal) > 0 {
+					// fmt.Println(elf.y, elf.x, "proposes", proposal[0], proposal[1])
+					elf.proposedY = proposal[0]
+					elf.proposedX = proposal[1]
+					elf.hasProposal = true
+
+					proposalKey := createKey(proposal[0], proposal[1])
+					_, pe := proposals[proposalKey]
+					if !pe {
+						proposals[proposalKey] = 1
+					} else {
+						proposals[proposalKey] += 1
+					}
+				} else {
+					elf.hasProposal = false
+				}
+			}
+
+			elfs[k] = elf
+		}
+
+		if len(proposals) == 0 {
+			break
+		}
+
+		newElfs := make(map[string]Elf)
+		for _, elf := range elfs {
+			if elf.hasProposal {
+				p, pe := proposals[createKey(elf.proposedY, elf.proposedX)]
+				if pe && p == 1 {
+					elf.y = elf.proposedY
+					elf.x = elf.proposedX
+				}
+				elf.hasProposal = false
+			}
+
+			if elf.y > maxY {
+				maxY = elf.y
+			}
+			if elf.y < minY {
+				minY = elf.y
+			}
+
+			if elf.x > maxX {
+				maxX = elf.x
+			}
+
+			if elf.x < minX {
+				minX = elf.x
+			}
+
+			_, elfExists := newElfs[createKey(elf.y, elf.x)]
+			if elfExists {
+				panic("WTF")
+			}
+
+			newElfs[createKey(elf.y, elf.x)] = elf
+
+		}
+
+		elfs = newElfs
+
+		globalDir += 3
+		index++
+		// show(elfs, maxY, maxX, minY, minX)
+	}
+
+	// show(elfs, maxY, maxX, minY, minX)
+
+	// 3882
+	// 1116
+
+	return index
 }
 
 func main() {
