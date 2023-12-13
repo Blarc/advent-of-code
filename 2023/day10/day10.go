3e3ba28040eea79d49dd310178258d227080014b
Blarc <jakob.malezic@gmail.com>
2023-12-13 20:55:58 +0100

    feat: add 2023/day10.go (part 2)

--- 2023/day10/day10.go
+++ 2023/day10/day10.go
@@ -18,9 +18,40 @@ var sample string
 var input string
 
 type Node struct {
-	y int
-	x int
-	t string
+	y    int
+	x    int
+	t    string
+	loop bool
+}
+
+func findStart(start *Node, m map[int]map[int]*Node) string {
+	up, _ := m[start.y-1][start.x]
+	down, _ := m[start.y+1][start.x]
+	right, _ := m[start.y][start.x+1]
+	left, _ := m[start.y][start.x-1]
+
+	upValid := up != nil && (up.t == "F" || up.t == "|" || up.t == "7")
+	downValid := down != nil && (down.t == "J" || down.t == "|" || down.t == "L")
+	rightValid := right != nil && (right.t == "J" || right.t == "-" || right.t == "7")
+	leftValid := left != nil && (left.t == "F" || left.t == "-" || left.t == "L")
+
+	if upValid && downValid {
+		return "|"
+	} else if upValid && rightValid {
+		return "L"
+	} else if upValid && downValid {
+		return "|"
+	} else if upValid && leftValid {
+		return "J"
+	} else if downValid && rightValid {
+		return "F"
+	} else if downValid && leftValid {
+		return "7"
+	} else if rightValid && leftValid {
+		return "-"
+	} else {
+		panic("No matches?")
+	}
 }
 
 func next(current *Node, previous *Node, m map[int]map[int]*Node) (*Node, *Node) {
@@ -47,6 +78,10 @@ func next(current *Node, previous *Node, m map[int]map[int]*Node) (*Node, *Node)
 	}
 }
 
+func coloredString(text string, color int) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", color, text)
+}
+
 // |
 // -
 // L
@@ -63,7 +98,7 @@ func part1(input string) int {
 	for y, l := range lines {
 		m[y] = make(map[int]*Node)
 		for x, c := range l {
-			m[y][x] = &Node{y, x, string(c)}
+			m[y][x] = &Node{y, x, string(c), false}
 			if c == 'S' {
 				start = m[y][x]
 			}
@@ -90,22 +125,46 @@ func part2(input string) int {
 	for y, l := range lines {
 		m[y] = make(map[int]*Node)
 		for x, c := range l {
-			m[y][x] = &Node{y, x, string(c)}
+			m[y][x] = &Node{y, x, string(c), false}
 			if c == 'S' {
 				start = m[y][x]
 			}
 		}
 	}
 
-	ans := 1
-	fmt.Println("Start: ", start)
+	start.loop = true
 	current, previous := next(start, nil, m)
 	for current.t != "S" {
+		current.loop = true
 		current, previous = next(current, previous, m)
-		ans++
 	}
 
-	return ans / 2
+	start.t = findStart(start, m)
+
+	ans := 0
+	for y := 0; y < len(lines); y++ {
+		inside := false
+		for x := 0; x < len(lines[y]); x++ {
+			node := m[y][x]
+			// Also works with (node.t == "|" || node.t == "J" || node.t == "L"); don't know why though...
+			if node.loop && (node.t == "|" || node.t == "F" || node.t == "7") {
+				inside = !inside
+			} else if !node.loop && inside {
+				ans += 1
+			}
+
+			//if node.loop {
+			//	fmt.Print(coloredString(node.t, 36))
+			//} else if !node.loop && inside {
+			//	fmt.Print(coloredString("I", 31))
+			//} else {
+			//	fmt.Print(coloredString("O", 34))
+			//}
+		}
+		//fmt.Println()
+	}
+
+	return ans
 }
 
 func main() {
@@ -125,6 +184,8 @@ func main() {
 		inputText = sample
 		fmt.Println("Running part", part, "on sample.txt.")
 	}
+
+	part2(strings.TrimSpace(inputText))
 
 	start := time.Now()
 	if part == 1 {
