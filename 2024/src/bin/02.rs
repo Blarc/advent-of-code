028fe417fa9408c8ddba67c0adc97ad4b87709c7
Blarc <jakob.malezic@gmail.com>
2024-12-02 08:25:08 +0100

    feat(2024): cleanup 02.rs

--- 2024/src/bin/02.rs
+++ 2024/src/bin/02.rs
@@ -22,59 +22,13 @@ pub fn is_safe(n: &Vec<u32>) -> bool {
             return false;
         }
     }
-
     true
 }
 
-pub fn remove_index(n: &Vec<u32>, index: usize) -> Vec<u32> {
-    n.iter()
-        .enumerate()
-        .filter_map(|(i, &x)| {
-            if i != index {
-                Some(x)
-            } else {
-                None
-            }
-        })
-        .collect()
-}
-
-pub fn is_safe_dampener(n: &Vec<u32>, dampener: bool) -> bool {
-    let mut increasing = None;
-    for i in 0..n.len()-1 {
-        let first = n[i];
-        let second = n[i+1];
-
-        // println!("{first}, {second}");
-        if first < second && first.abs_diff(second) <= 3 {
-            if let Some(false) = increasing {
-                return if dampener {
-                    is_safe_dampener(&remove_index(n, i), false) || is_safe_dampener(&remove_index(n, i+1), false)
-                } else {
-                    // println!("false 1");
-                    false
-                }
-            }
-            increasing = Some(true);
-        } else if first > second && first.abs_diff(second) <= 3 {
-            if let Some(true) = increasing {
-                return if dampener {
-                    is_safe_dampener(&remove_index(n, i), false) || is_safe_dampener(&remove_index(n, i+1), false)
-                } else {
-                    // println!("false 2");
-                    false
-                }
-            }
-            increasing = Some(false);
-        } else if dampener {
-            return is_safe_dampener(&remove_index(n, i), false) || is_safe_dampener(&remove_index(n, i+1), false)
-        } else {
-            // println!("false 3");
-            return false;
-        }
-    }
-
-    true
+pub fn copy_and_remove_index(n: &Vec<u32>, index: usize) -> Vec<u32> {
+    let mut new_vec = n.to_vec();
+    new_vec.remove(index);
+    new_vec
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -87,7 +41,6 @@ pub fn part_one(input: &str) -> Option<u32> {
             .collect();
 
         let safe = is_safe(&numbers);
-        println!("{:?}: {}", numbers, safe);
         if safe {
             sum += 1
         }
@@ -104,26 +57,17 @@ pub fn part_two(input: &str) -> Option<u32> {
             .filter_map(|s| u32::from_str(s).ok())
             .collect();
 
-        // let safe = is_safe_dampener(&numbers, true);
-        // println!("{:?}: {}", numbers, safe);
-        // if safe {
-        //     sum += 1
-        // }
-
         let mut safe = is_safe(&numbers);
         if !safe {
             for i in 0..numbers.len() {
-                safe = safe || is_safe(&remove_index(&numbers, i))
+                safe = safe || is_safe(&copy_and_remove_index(&numbers, i))
             }
         }
-        println!("{:?}: {}", numbers, safe);
         if safe {
             sum += 1
         }
     }
 
-    // 644 too low
-    // 655 too low
     Some(sum)
 }
 
